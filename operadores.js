55a0d8487bdc4b8f0291521be3f53e824c8e6efb
wullerbarros <wuller1987@gmail.com>
2025-04-23 17:03:56 -0300

    continuação

--- operadores.js
+++ operadores.js
@@ -3,16 +3,31 @@
 //divisão
 //multiplicação
 
-let numero1 = Number(prompt("Digite um numero"))
-let numero2 = Number(prompt("Digite outro numero"))
+// let numero1 = Number(prompt("Digite um numero"))
+// // let numero2 = Number(prompt("Digite outro numero"))
 
-let soma = numero1 + numero2
-let subtracao =  numero1 - numero2
-let divisao =  numero1 / numero2
-let multiplicacao =  numero1 / numero2
-let resto = numero1%numero2
+// let soma = numero1 + numero2
+// let subtracao =  numero1 - numero2
+// let divisao =  numero1 / numero2
+// let multiplicacao =  numero1 / numero2
+// let resto = numero1%numero2
 
-alert(`A soma é ${soma}, a subtração é ${subtracao}, a multiplicação é ${multiplicacao} e a divisão é ${divisao}, o resto é ${resto}`)
+// alert(`A soma é ${soma}, a subtração é ${subtracao}, a multiplicação é ${multiplicacao} e a divisão é ${divisao}, o resto é ${resto}`)
 
 
 //operadores de atribuiçao
+let dado = "alguma informação"
+let x = 5
+x = x + 5
+
+x += 5
+
+//operadores de comparação
+
+document.write(5/4)
+
+document.write(5%4)
+document.write(5-4)
+
+//operadores logicos (and-&&, ou - ||, diferente- !)
+
